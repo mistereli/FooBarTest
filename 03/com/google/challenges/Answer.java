000c6b8ff9d5f0fdaf53bdb0f271934281e03984
mistereli <mistereli@users.noreply.github.com>
2016-10-08 21:35:01 -0400

    Update Answer.java

--- 03/com/google/challenges/Answer.java
+++ 03/com/google/challenges/Answer.java
@@ -1,22 +1,22 @@
 package com.google.challenges;
 
 public class Answer {
-	public static int answer(int start, int length) {
-		
+    public static int answer(int start, int length) {
+        
         int count = start;
         int checksum = -1;
 
         for(int y = length - 1; y > -1; y--)
-		{
+        {
             int line = y + 1;
 
             for(int x = 0; x < length + 1; x++)
-			{
+            {
                 if(line > 0)
-				{
+                {
                     
-					if(checksum > -1)
-					{
+                    if(checksum > -1)
+                    {
                         checksum ^= count;
                     } else {
                         checksum = start;
@@ -24,7 +24,7 @@ public class Answer {
 
                     line--;
                     count++;
-					
+                    
                 } else {
                     count += length - x;
                     break;
@@ -32,23 +32,23 @@ public class Answer {
             }
         }
 
-		System.out.println("final = " + Integer.toString(checksum));
+        System.out.println("final = " + Integer.toString(checksum));
         
-		return checksum;
-		
+        return checksum;
+        
     }
 
-	public static void main(String[] args)
-	{		
+    public static void main(String[] args)
+    {       
 
-		if (args.length == 2)
-		{
+        if (args.length == 2)
+        {
 
-			answer(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-		} else {
-			System.out.println("Requires 2 arguments");
-		}
-	
-	}
+            answer(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        } else {
+            System.out.println("Requires 2 arguments");
+        }
+    
+    }
 
 }
